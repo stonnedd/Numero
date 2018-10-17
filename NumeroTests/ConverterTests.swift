5080bfb496b769cbc6c259b42d03a12f90a52e3e
Yonadab <yonadab.lopez@gmail.com>
2018-10-17 06:55:55 -0400

    Initial Commit

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,31 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by MCS on 10/16/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  
+    let converter = Converter()
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+  func testConertionForOne() {
+    let result = converter.convert(1)
+  }
+  
+  
+  
+
+}
